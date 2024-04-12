f623314634b33fdec48b13e074d72c9d250b42d3
Ishita <ishita@gluelabs.com>
2024-04-13 00:37:52 +0530

    added styling

--- reflex_app/reflex_app.py
+++ reflex_app/reflex_app.py
@@ -33,23 +33,13 @@ def chat() -> rx.Component:
         *[
             qa(question, answer)
             for question, answer in qa_pairs
-        ]
+        ],
+        class_name="text-3xl"
     )
 
 
 def index() -> rx.Component:
-    return rx.container(
-        rx.box(
-            "What is Reflex?",
-            # The user's question is on the right.
-            text_align="right",
-        ),
-        rx.box(
-            "A way to build web apps in pure Python!",
-            # The answer is on the left.
-            text_align="left",
-        ),
-    )
+    return rx.container(chat())
 
 
 app = rx.App()
--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,14 @@
 import reflex as rx
 
+class AppConfig(rx.Config):
+    pass
+
 config = rx.Config(
     app_name="reflex_app",
+    tailwind={
+        "theme": {
+            "extend": {},
+        },
+        "plugins": ["@tailwindcss/typography"],
+    },
 )
